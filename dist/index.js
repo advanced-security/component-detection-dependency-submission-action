d967f286bb9ddb1c3967d139e4b38273d990cd27
Justin Hutchings <jhutchings1@users.noreply.github.com>
2023-01-20 01:43:53 +0000

    Fix type errors

--- dist/index.js
+++ dist/index.js
@@ -23887,6 +23887,29 @@ exports["default"] = CondaParser;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -23900,20 +23923,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
-const github_1 = __importDefault(__nccwpck_require__(5438));
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
 const dependency_submission_toolkit_1 = __nccwpck_require__(9810);
 const condaParser_1 = __importDefault(__nccwpck_require__(2120));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        let manifests = condaParser_1.default.getManifestsFromEnvironmentFiles(condaParser_1.default.searchFiles(core_1.default.getInput('filePath'), core_1.default.getInput('filePattern')));
+        let manifests = condaParser_1.default.getManifestsFromEnvironmentFiles(condaParser_1.default.searchFiles(core.getInput('filePath'), core.getInput('filePattern')));
         let snapshot = new dependency_submission_toolkit_1.Snapshot({
             name: "conda-dependency-submission-action",
             version: "0.0.1",
             url: "https://github.com/jhutchings1/conda-dependency-submission-action",
-        }, github_1.default.context, {
-            correlator: `${github_1.default.context.job}`,
-            id: github_1.default.context.runId.toString()
+        }, github.context, {
+            correlator: `${github.context.job}`,
+            id: github.context.runId.toString()
         });
         manifests === null || manifests === void 0 ? void 0 : manifests.forEach(manifest => {
             snapshot.addManifest(manifest);
