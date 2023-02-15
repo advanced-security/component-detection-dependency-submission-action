2d39af112c3c928b63a49b43890a06faa5d29496
Justin Hutchings <jhutchings1@users.noreply.github.com>
2023-02-15 00:22:28 +0000

    Fix tests

--- componentDetection.test.ts
+++ componentDetection.test.ts
@@ -2,16 +2,19 @@ import ComponentDetection from './componentDetection';
 import fs from 'fs';
 
 test('Downloads CLI', async () => {
-  ComponentDetection.downloadLatestRelease();
+  await ComponentDetection.downloadLatestRelease();
   expect(fs.existsSync(ComponentDetection.componentDetectionPath));
 });
 
 test('Runs CLI', async () => {
-   await ComponentDetection.runComponentDetection('./test');
-   expect(fs.existsSync(ComponentDetection.outputPath));
+  await ComponentDetection.downloadLatestRelease();
+  await ComponentDetection.runComponentDetection('./test');
+  expect(fs.existsSync(ComponentDetection.outputPath));
 });
 
 test('Parses CLI output', async () => {
+  await ComponentDetection.downloadLatestRelease();
+  await ComponentDetection.runComponentDetection('./test');
   var manifests = await ComponentDetection.getManifestsFromResults();
   expect(manifests?.length == 2);
 });
